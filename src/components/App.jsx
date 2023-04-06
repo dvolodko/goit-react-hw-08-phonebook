727483ae914ca1b7b215299fbaaad7ab1a408e02
Denys Volodko <babyleejane@gmail.com>
2023-04-06 16:50:24 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,6 @@ export class App extends Component {
   state = {
     contacts: [
       { name: 'Rosie Simpson', number: '459-12-56', id: 'id-1' },
-      { name: 'Ashley Admas', number: '457-17-57', id: 'id-5' },
       { name: 'Hermione Kline', number: '443-89-12', id: 'id-2' },
       { name: 'Eden Clements', number: '645-17-79', id: 'id-3' },
       { name: 'Annie Copeland', number: '227-91-26', id: 'id-4' },
